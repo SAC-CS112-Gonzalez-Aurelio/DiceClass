f70b1dbcc7856ea299a4d421fda1874956bafb52
Aurelio Gonzalez <aureliog213@gmail.com>
2015-11-04 20:12:40 -0800

    DiceClass

---
+++ src/DiceClass.java
@@ -0,0 +1,17 @@
+
+public class DiceClass {
+	private int value;
+	
+	public int Throw()
+	{
+		int rand10 = 1 + (int)(Math.random() * 6);
+		rand10 = value;
+		return value;
+	}
+	public int Value()
+	{
+	return value;
+	
+			}
+			}
+
